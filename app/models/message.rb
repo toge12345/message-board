0cca837364ae7cba9d6dc5a3c31dce06909ad244
shimizu_megumi <kokokara5metre@gmail.com>
2015-11-08 13:24:31 +0000

    age-add

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ActiveRecord::Base
     #名前は必須入力かつ20文字以内
     validates :name , length: { maximum:20 } ,presence: true
-    validates :age , length: { maximum:120 } ,numericality: { only_integer:true,greater_than_or_equal_to:0},presence: true    
+    validates :age , length: { maximum:3 } ,numericality: { only_integer:true,greater_than_or_equal_to:0,less_than_or_equal_to:120},presence: true    
     validates :body , length: { minimum:2 , maximum: 30 } , presence: true   
     
 end
